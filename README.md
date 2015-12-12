2d8b4bb5698c15f6f712a911450a66f16c447246
jmz <jmz-b@users.noreply.github.com>
2015-12-12 00:00:07 +0000

    overhaul interface to be inline at the terminal (no new tab)
        - invoke with a shell script/alias
        - control current shell via command line (feedChild())

--- README.md
+++ README.md
@@ -1,31 +1,18 @@
 # termit-lscommander
+
 [Termit](https://github.com/nonstop/termit) plugin to navigate directories and open files with a mouse
 
-![alt tag](https://raw.githubusercontent.com/jmz-b/termit-lscommander/master/demo.gif)
 
 ## Installation and Configuration
+
 Download the script
 
     curl https://raw.githubusercontent.com/jmz-b/termit-lscommander/master/lscommander.lua >  $HOME/.config/termit/lscommander.lua
 
-1 ) Add the following set up code the the beginning of your rc.lua
+1) Copy ./lsc into your $PATH. Alternately set this alias:
 
-```
-require('os')
-require('termit/lscommander')
-
-home = os.getenv('HOME')
-Commander.popupDirs = {
-	home = home,
-	src = home .. '/src',
-}
-Commander:init()
-```
+    alias lsc="ls -ld  --group-directories-first --color $PWD/{*,.*} | sed 's/^/== /'"
 
 2) Add the following to your defaults.matches in rc.lua
 
     [Commander.pattern] = function(lsstr) Commander:run(lsstr) end
-
-3) If you want to be able to access the browser from a shell, you can set an alias like this:
-
-    alias lscommander="bash -c 'ls -ld  --group-directories-first --color $PWD/{*,.*} | sed \"s/^/== /\" | less -R && bash'"
--- lscommander.lua
+++ lscommander.lua
@@ -1,15 +1,7 @@
 require('os')
 
 Commander = {
-	-- open a new shell and display long ls output in less
-	spawnBrowser = "bash -c 'ls -ld  --group-directories-first --color $PWD/{*,.*} | sed \"s/^/== /\" | less -R && bash'",
-	spawnViewer = 'xdg-open',
-
 	pattern = '^== [drwx-]{10}+.*',
-	newTab = false,
-	popupDirs = {
-		home = os.getenv('HOME')
-	},
 }
 
 -- serialize a line of ls output into a table
@@ -32,76 +24,7 @@ end
 -- return True if path is a directory
 -- return False path is a file
 Commander.isDir = function (perms)
-	if perms:sub(1, 1) == 'd' then
-		return true
-	else
-		return false
-	end
-end
-
--- factory to generate parameters for openTab
-Commander.getTabInfo = function (self, path)
-	return {
-		command = self.spawnBrowser,
-		title = path,
-		workingDir = path,
-	}
-end
-
--- produce a string to use as a seperator in the popup menu
--- based on the length of another string
-Commander.getSeperator = function (initialSeperator, str)
-	local seperator = initialSeperator
-	for i = 1, string.len(str) do
-		if string.len(seperator) >= string.len(str) then
-			return seperator
-		end
-		seperator = seperator .. '-'
-	end
-end
-
--- open lscommander tab
-Commander.openBrowser = function (self, path)
-	openTab(self:getTabInfo(path))
-
-	if not self.newTab then
-		prevTab()
-		closeTab()
-	end
-end
-
--- populate the popup menu
-Commander.init = function (self)
-	local lscommanderMenu = {}
-	local newTabText = 'Open in new tab'
-	local currentTabText = 'Open in current tab'
-	local initialSeperator = self.getSeperator('-', currentTabText)
-	local longestTitle= initialSeperator
-
-	for title, path in pairs(self.popupDirs) do
-		if string.len(title) > string.len(longestTitle) then
-			longestTitle = title
-		end
-
-		table.insert(lscommanderMenu, {
-			name=title,
-			action=function () self:openBrowser(path) end,
-		})
-	end
-
-	table.insert(lscommanderMenu, {
-		name=self.getSeperator(initialSeperator, longestTitle)
-	})
-	table.insert(lscommanderMenu, {
-		name=newTabText,
-		action=function() self.newTab = true end,
-	})
-	table.insert(lscommanderMenu, {
-		name=currentTabText,
-		action=function() self.newTab = false end,
-	})
-
-	addPopupMenu(lscommanderMenu, "lscommander")
+	if perms:sub(1, 1) == 'd' then return true else return false end
 end
 
 Commander.run = function (self, lsstr)
@@ -110,8 +33,8 @@ Commander.run = function (self, lsstr)
 	local path = Commander.getPath(line)
 
 	if Commander.isDir(perms) then
-		Commander:openBrowser(path)
+		feedChild('\rcd ' .. path .. ' && lsc\r')
 	else
-		os.execute(self.spawnViewer .. ' ' ..  path)
-	end
+		os.execute('xdg-open ' ..  path)
+	end	
 end
