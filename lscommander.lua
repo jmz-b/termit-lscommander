e4d80967dcc505fc8008f3e2d2a376924c0ba18c
jmz <jmz-b@users.noreply.github.com>
2015-12-13 21:17:42 +0000

    less is more

--- lscommander.lua
+++ lscommander.lua
@@ -4,35 +4,14 @@ Commander = {
 	pattern = '^== [drwx-]{10}+.*',
 }
 
--- serialize a line of ls output into a table
-Commander.loadLine = function (lsstr)
-	local cols = {}
-	for c in lsstr:gmatch("%S+") do table.insert(cols, c) end
-	return cols
-end
-
--- return the permission string from a line table
-Commander.getPerms = function (line)
-	return line[2]
-end
-
--- return the path in a line table
-Commander.getPath = function (line)
-	return line[10]
-end
-
--- return True if path is a directory
--- return False path is a file
-Commander.isDir = function (perms)
-	if perms:sub(1, 1) == 'd' then return true else return false end
-end
-
 Commander.run = function (self, lsstr)
-	local line = Commander.loadLine(lsstr)
-	local perms = Commander.getPerms(line)
-	local path = Commander.getPath(line)
+	local line = {}
+	for col in lsstr:gmatch("%S+") do table.insert(line, col) end
 
-	if Commander.isDir(perms) then
+	local perms = line[2]
+	local path = line[10]
+
+	if perms:sub(1, 1) == 'd' then
 		feedChild('\rcd ' .. path .. ' && lsc\r')
 	else
 		os.execute('xdg-open ' ..  path)
